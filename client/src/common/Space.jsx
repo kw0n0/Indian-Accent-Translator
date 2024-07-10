e320ff3ced9d7c588c34b7a69ebe6a78c1d78fc1
kw0n0 <alwayskw0n0@gmail.com>
2024-07-10 23:37:26 +0900

    fix: Space 공통 컴포넌트 내 margin을 padding으로 변경

--- client/src/common/Space.jsx
+++ client/src/common/Space.jsx
@@ -5,10 +5,10 @@ export function Space({ top, right, bottom, left }) {
   return (
     <div
       css={css`
-        margin-top: ${top}px;
-        margin-right: ${right}px;
-        margin-bottom: ${bottom}px;
-        margin-left: ${left}px;
+        padding-top: ${top}px;
+        padding-right: ${right}px;
+        padding-bottom: ${bottom}px;
+        padding-left: ${left}px;
       `}
       aria-hidden="true"
     />
